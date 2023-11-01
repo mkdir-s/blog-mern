c9a763f41598e2a18104eb3cac6c5b829f5803d1
artem alexeenko <artyom.alexeyenko@gmail.com>
2023-11-01 12:31:29 +0600

    connect mangoDb, add user controller, user scheme, validator

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+import {registerValidation} from './validations/auth.js';
+
+import checkAuth from './utils/checkAuth.js';
+import * as UserController from './contollers/UserController.js';
+import User from './models/User.js';
+
+mongoose.connect('mongodb+srv://admin:www@cluster0.ozr5ir1.mongodb.net/blog?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('DB ok')
+  })
+  .catch((err) => {
+    console.log('DB error', err)
+  });
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/auth/login', UserController.login)
+
+app.post('/auth/register', registerValidation, UserController.register);
+
+app.get('/auth/me', checkAuth, UserController.getMe)
+
+app.listen(4444, (err) => {
+  if (err) {
+    return console.log(err)
+  }
+  
+  console.log('server ok')
+});
